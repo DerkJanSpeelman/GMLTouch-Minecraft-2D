7580663288216cdc0c6525dc6d7b4280007071ea
Derk Jan Speelman <derkjanspeelman@gmail.com>
2020-02-27 18:33:46 +0100

    Add documentation

--- Episode 6 Infinite Random Terrain Generation/src/scripts/PRNG.ts
+++ Episode 6 Infinite Random Terrain Generation/src/scripts/PRNG.ts
@@ -7,6 +7,11 @@
  * https://en.wikipedia.org/wiki/Lehmer_random_number_generator
  * 
  */
+
+/**
+ * @exports
+ * @class PRNG
+ */
 export class PRNG {
 
     public seed: number;
@@ -14,57 +19,75 @@ export class PRNG {
     public m: number;
     public a: number;
 
+    /**
+     * Creates a PRNG instance
+     * 
+     * @param {number | string} seed
+     */
     constructor(seed?: number | string) {
-
         this.m = 2147483647;
         this.a = 16807;
 
         let s: number;
 
         if (seed === undefined) {
-            
             s = this.calc(Math.random());
-
         } else {
-            
             if (typeof seed === 'string') {
-
                 let n: number = 0;
 
                 for (let i = 0; i < seed.length; i++) {
-                    
                     n += seed.charCodeAt(i);
                 }
 
                 s = this.calc(n);
-
             } else {
-
                 s = seed;
             }
         }
 
         this.seed = s % this.m;
-
         if (this.seed <= 0) this.seed += this.m - 1;
 
         this.shift();
     }
 
+    /**
+     * Calculates a "random" number based on its input
+     * 
+     * @public
+     * @readonly
+     * 
+     * @param   {number} n
+     * @returns {number} Math.round( n * this.m - 1 )
+     */
     public readonly calc: ((
         n: number
     ) => number) = (
         n: number
     ): number => {
-
         return Math.round(n * this.m - 1);
     }
 
+    /**
+     * Shifts the seed to a different seed.
+     * 
+     * @public
+     * @readonly
+     * @void
+     */
     public readonly shift: (() => void) = (): void => {
-
         this.seed = this.seed * this.a % this.m;
     }
 
+    /**
+     * Shifts the seed to a different seed.
+     * 
+     * @public
+     * @readonly
+     * @param {number|undefined} minOrMax
+     * @returns {number|undefined}
+     */
     public readonly next: ((
         minOrMax?: number,
         max?: number
@@ -72,28 +95,28 @@ export class PRNG {
         minOrMax?: number,
         max?: number
     ): number => {
-
         this.shift();
 
         let min: number | undefined = minOrMax;
 
-        if (min === undefined) min = 0;
+        if (min === undefined) {
+            min = 0;
+        }
 
         if (max === undefined) {
-
             max = min;
             min = 0;
         }
 
         if (min === 0 || (max === undefined || max === 0 || max === 1)) {
-
             return this.seed / this.m;
         }
 
-        if (min === max) return this.seed / this.m + min;
+        if (min === max){
+            return this.seed / this.m + min;
+        }
 
         if (min > max) {
-
             minOrMax = max;
             max = min;
             min = minOrMax;
